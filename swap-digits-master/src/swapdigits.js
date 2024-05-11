835fabde41fda3cddcb09c6e0cff4504ab67fad0
amit-negi-23 <amitnegiit23@gmail.com>
2024-05-11 18:04:34 +0530

    week 1  (completed)

--- swap-digits-master/src/swapdigits.js
+++ swap-digits-master/src/swapdigits.js
@@ -1,8 +1,30 @@
-const swapDigits = (number)=>{
-    let swappedNumber = 0
-    //write logic here
-    return swappedNumber;
+const swapDigits = (number) => {
+    if (number < 0) {
+        swappedNumber = 0;
+        return swappedNumber;
+    }else{
+        let numStr = number.toString();
+        let length = numStr.length;
+        let swappedNumber = "";
     
+        if (length % 2 == 0) {
+            for (let i = 0; i < length; i += 2) {
+                swappedNumber += numStr[i + 1] + numStr[i]
+            }
+            return parseInt(swappedNumber);
+        } else if(length ==1){
+            swappedNumber = 0;
+            return swappedNumber;
+        }else if (length % 2 !== 0 && number > 0) {
+            swappedNumber = numStr[0]
+            for (let i = 1; i < length; i += 2) {
+                swappedNumber += numStr[i + 1] + numStr[i]
+            }
+            return parseInt(swappedNumber);
+        }
+    }
+    
+
 }
 
 module.exports = swapDigits
